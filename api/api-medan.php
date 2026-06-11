f3713a938ec2a78306f63233058bd6d0e523b099
pulzz-store <aset2466@gmail.com>
2026-06-11 15:08:59 +0700

    Update api-medan.php

--- api/api-medan.php
+++ api/api-medan.php
@@ -1,7 +1,19 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD']!== 'POST') {
+    echo json_encode(['error' => 'Method not allowed']);
+    exit;
+}
+//... sisa kode lu
 $MEDAN_API_ID = '50098';
 $MEDAN_API_URL = 'https://api.medanpedia.co.id';
 $action = $_POST['action']?? '';
